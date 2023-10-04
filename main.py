243b9c9c19a59459b2c57e275377a1f84bbde000
Micahaha <xicer029@gmail.com>
2023-10-04 10:45:52 -0400

    Finis

---
+++ main.py
@@ -0,0 +1,8 @@
+from account.account import *
+
+def main():
+    # Create an account object named a1 that has a balance of $100.00
+    a1 = account(100.0, 100.0)
+
+if __name__ == "__main__":
+    main()
--- src/account/account.py
+++ src/account/account.py
@@ -1,5 +1,15 @@
 from transaction.transaction import *
 
 class account(transaction):
-    def __init__(self, *args) -> None:
-        super().__init__()
+    def __init__(self, *args):
+        print(args)
+        if(len(args) == 1):
+            try:
+                if(args[0] < 0.0):
+                    raise ValueError("Balance is less than zero.")
+            except ValueError as e:
+                exit(e)
+            finally:
+                self._balance = float(args[0])
+        else:
+            self._balance = 0.0 
